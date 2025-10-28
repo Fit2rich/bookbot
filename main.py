dcc61f402a00d23983dacef9090a10e342da04cb
Fit2rich <george.panagiotakopoulos28@gmail.com>
2025-10-28 07:22:24 +0200

    Update main.py and stats.py with character report and CLI arguments

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
-from stats import count_words
+import sys
+from stats import count_words, count_characters, chars_dict_to_sorted_list
 
 
-
-def get_boot_text(filepath):
-    with open(filepath,'r' ) as f:
+def get_book_text(filepath):
+    with open(filepath, 'r') as f:
         text = f.read()
     return text
 
 
-
-
-
 def main():
-    filepath = 'books/frankenstein.txt'
-    book_text = get_boot_text(filepath)
+   
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+
+    book_text = get_book_text(filepath)
+
+    print("----------- Word Count ----------")
     num_words = count_words(book_text)
     print(f"Found {num_words} total words")
 
+    print("--------- Character Count -------")
+    char_counts = count_characters(book_text)
+    sorted_chars = chars_dict_to_sorted_list(char_counts)
 
+    for item in sorted_chars:
+        char = item["char"]
+        if char.isalpha():  # Only print letters
+            print(f"{char}: {item['num']}")
 
+    print("============= END ===============")
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- stats.py
+++ stats.py
@@ -2,3 +2,29 @@ def count_words(text):
     words = text.split()
     num_words  = len(words)
     return num_words
+
+
+
+def count_characters(text):
+    char_counts = {}
+    text = text.lower()
+
+    for char in text:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+
+    return char_counts
+
+def sort_on(item):
+    return item["num"]
+
+
+def chars_dict_to_sorted_list(char_dict):
+    sorted_list = []
+    for char, num in char_dict.items():
+        sorted_list.append({"char": char, "num": num})
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
